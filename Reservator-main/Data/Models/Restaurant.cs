1028c758a0c63246951b4970fe4bfbfdee5faeaf
DQDO <BABAB>
2023-03-23 12:32:05 +0200

    add

---
+++ Reservator-main/Data/Models/Restaurant.cs
@@ -0,0 +1,28 @@
+﻿using Reservator.Data.Models;
+
+namespace Reservator.Models
+{
+    public class Restaurant : BaseEntity
+    {
+        public Restaurant()
+        {
+            Places = new HashSet<Place>();
+            RestaurantUserGrades = new HashSet<RestaurantUserGrade>();
+            Images = new HashSet<Image>();
+        }
+        public string Name { get; set; }
+        public Town? Town { get; set; }
+        public string Address { get; set; }
+        public string? MainPic { get; set; }
+        public string? Pictures { get; set; }
+        public double? Rating { get; set; }
+        public double? Price { get; set; }
+
+        public virtual ICollection<Image> Images { get; set; }
+        public virtual ICollection<Place> Places { get; set; }
+        public virtual ICollection<RestaurantUserGrade> RestaurantUserGrades { get; set; }
+    }
+
+
+
+}
---
+++ Reservator-main/Migrations/20230308111505_Restaurant3.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Reservator.Migrations
+{
+    public partial class Restaurant3 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "Price",
+                table: "Restaurants",
+                type: "float",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Price",
+                table: "Restaurants");
+        }
+    }
+}
